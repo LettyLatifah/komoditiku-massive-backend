c370c35efeac470782837c5b1750bef146fb0639
herysuwardi <herysuwardi6@gmail.com>
2023-01-18 16:51:30 +0700

    Menambahkan CRUD pada API artikel

--- src/controller/artikel.js
+++ src/controller/artikel.js
@@ -49,11 +49,75 @@ const getArticleById = async (req, res) => {
   }
 };
 
-const createNewArticle = () => {};
+const createNewArticle = async (req, res) => {
+  const { file, body } = req;
 
-const updateArticle = () => {};
+  if (req.file === undefined)
+    return res.status(400).json({ message: 'Foto Artikel dibutuhkan!' });
 
-const deleteArticle = () => {};
+  const data = {
+    ...body,
+    foto_produk: file.filename,
+    // foto_umkm: file.filename,
+    // multiple: fileName
+  };
+
+  try {
+    await cardModel.createNewArticle(data);
+    res.status(201).json({
+      message: 'Add Card Article Success',
+      data,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: 'Server Error',
+      serverMessage: error,
+    });
+  }
+};
+
+const updateArticle = async (req, res) => {
+  const { idArticle } = req.params;
+  const { file, body } = req;
+
+  if (req.file === undefined)
+    return res.status(400).json({ message: 'Foto artikel dibutuhkan!' });
+
+  const data = {
+    ...body,
+    foto_artikel: file.filename,
+  };
+
+  try {
+    await productModel.updateArticle(data, idArticle);
+    res.json({
+      message: 'Update Article Success',
+      data: data,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: 'Server Error',
+      serverMessage: error,
+    });
+  }
+};
+
+const deleteArticle = async (req, res) => {
+  const { idArticle } = req.params;
+
+  try {
+    await productModel.deleteArticle(idArticle);
+    res.json({
+      message: 'Delete Article Success',
+      data: null,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: 'Server Error',
+      serverMessage: error,
+    });
+  }
+};
 
 module.exports = {
   articleValidator,
